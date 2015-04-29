15e1e33763b039f105acb6e1c5fa9f7047e302dd
Anirud <anirudt@gmail.com>
2015-04-29 16:17:56 +0530

    added script file, for system calls

---
+++ script.pl
@@ -0,0 +1,7 @@
+use strict;
+use warnings;
+
+#my $s1 = system("python -c 'print len(\'1+1\')'");
+#my $s2 = system("cat out.csv");
+my $s3 = system("python -c 'import trial'");
+
---
+++ trial.py
@@ -0,0 +1,9 @@
+a = 0
+b = 0
+print "Inside the python file"
+print "Getting out"
+
+def fun(n):
+	return n*n+1
+
+
